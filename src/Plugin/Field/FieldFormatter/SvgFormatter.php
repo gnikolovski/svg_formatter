37f464ea9cb2900e1c45675d50e3e1151c77e50d
Goran Nikolovski <nikolovski84@gmail.com>
2017-01-17 20:52:24 +0100

    Initial commit

---
+++ src/Plugin/Field/FieldFormatter/SvgFormatter.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\svg_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'svg_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "svg_formatter",
+ *   label = @Translation("Svg formatter"),
+ *   field_types = {
+ *     "file"
+ *   }
+ * )
+ */
+class SvgFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+      'apply_dimensions' => TRUE,
+      'width' => 16,
+      'height' => 16,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+
+    $form['apply_dimensions'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Apply dimensions.'),
+      '#default_value' => $this->getSetting('apply_dimensions'),
+    );
+    $form['width'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Image width.'),
+      '#default_value' => $this->getSetting('width'),
+      '#element_validate' => array('element_validate_integer_positive'),
+      '#size' => 5,
+    );
+    $form['height'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Image height.'),
+      '#default_value' => $this->getSetting('height'),
+      '#element_validate' => array('element_validate_integer_positive'),
+      '#size' => 5,
+    );
+    
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+    if ($this->getSetting('apply_dimensions') && $this->getSetting('width')) {
+      $summary[] = $this->t('Image width:') . ' ' . $this->getSetting('width');
+    }
+    if ($this->getSetting('apply_dimensions') && $this->getSetting('width')) {
+      $summary[] = $this->t('Image height:') . ' ' . $this->getSetting('height');
+    }
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $apply_dimensions = $this->getSetting('apply_dimensions');
+    $width = $this->getSetting('width');
+    $height = $this->getSetting('height');
+
+    foreach ($items as $delta => $item) {
+      if ($item->entity) {
+        $uri = $item->entity->getFileUri();
+        $elements[$delta] = array(
+          '#theme' => 'svg_formatter',
+          '#uri' => $uri,
+          '#apply_dimensions' => $apply_dimensions,
+          '#width' => $width,
+          '#height' => $height,
+        );
+      }
+    }
+
+    return $elements;
+  }
+
+}
