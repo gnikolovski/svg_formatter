22c1f9962059b88082a38cd7d11c8e77cb2ed7e1
Goran Nikolovski <nikolovski84@gmail.com>
2024-12-31 08:40:47 +0100

    Issue #3470209 by rajan kumar@2026, flyke, berdir, gnikolovski: Make compatible with Drupal 11

--- tests/src/Functional/SvgFormatterTest.php
+++ tests/src/Functional/SvgFormatterTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\svg_formatter\Functional;
 
 use Drupal\Core\File\Exception\FileException;
+use Drupal\Core\File\FileExists;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\media\Entity\Media;
@@ -132,7 +133,7 @@ class SvgFormatterTest extends BrowserTestBase {
     }
 
     $source_file = \Drupal::service('file.repository')
-      ->writeData(self::SVG_DATA, $destination, FileSystemInterface::EXISTS_REPLACE);
+      ->writeData(self::SVG_DATA, $destination, FileExists::Replace);
 
     return $source_file;
   }
